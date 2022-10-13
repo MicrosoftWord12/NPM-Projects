2775aa95247879680034994d0eb50be8d89e53da
MicrosoftWord12 <mrjacobbrooks@gmail.com>
2022-10-13 09:33:14 +0100

    File Manager, Loader

---
+++ src/lib/FileLoader/FileLoader.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+const envFile = path.resolve(__dirname, "../Templates/Configs/.env");
+const jsonFile = path.resolve(__dirname, "../Templates/Configs/Config.json");
+
+const botFile = path.resolve(__filename);
+
+const createEnvFile = () => {
+    fs;
+};
+
+const createJsonFile = () => {};
---
+++ src/lib/FileLoader/FileManager.js
@@ -0,0 +1,14 @@
+export class FileManager {
+    constructor() {
+        this.fileLoader = this.files = [];
+        this.createdFiles = [];
+    }
+
+    getFiles() {
+        return this.files;
+    }
+
+    getCreatedFiles() {
+        return this.createdFiles;
+    }
+}
