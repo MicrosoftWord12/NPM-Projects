0dda5cecd4cef97ed461d0214e43d7fbc795a802
MicrosoftWord12 <mrjacobbrooks@gmail.com>
2022-10-13 09:33:35 +0100

    Removed index.js JSON Comment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const cp = require("child_process");
 const readline = require("readline");
+const { FileManager } = require("./lib/FileLoader/FileManager");
+const DirectoryManager = require("./lib/DirectoryTree/DirectoryManager");
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -13,7 +15,6 @@ export function main() {
             rl.question("Would you like a JSON Config File or a .env file?", (answer) => {
                 answer = answer.toLowerCase();
                 if (answer === "json") {
-                    //Json File Config
                 } else {
                     // .env File Config
                 }
